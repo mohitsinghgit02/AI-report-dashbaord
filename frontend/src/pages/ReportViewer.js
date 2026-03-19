52af256501fa48368772eb23ba2eb66326fb3538
Mohit Singh <mohit.singh02@nagarro.com>
2026-03-19 11:24:07 +0000

    Docker compose added

--- frontend/src/pages/ReportViewer.js
+++ frontend/src/pages/ReportViewer.js
@@ -112,7 +112,7 @@ export default function ReportViewer() {
         setLang(urlLang);
 
         const api =
-            `http://localhost:8000/api/report?file=${file}&lang=${urlLang}`;
+            `${process.env.REACT_APP_API_URL}/report?file=${file}&lang=${urlLang}`;
 
         fetch(api)
             .then(res => res.json())
